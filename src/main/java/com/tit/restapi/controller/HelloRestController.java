3155af7b26ab8a06e0f3d2fa8dd9449282f74823
AdityaRaj00007 <133851674+AdityaRaj00007@users.noreply.github.com>
2025-02-22 13:36:25 +0530

    [Aditya] Add . Create a Rest Controller to demonstrate

--- src/main/java/com/tit/restapi/controller/HelloRestController.java
+++ src/main/java/com/tit/restapi/controller/HelloRestController.java
@@ -1,4 +1,4 @@
-package com.capgemini_training.RestCallApi.controller;
+package com.tit.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
